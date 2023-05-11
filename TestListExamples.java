8cbc7caa63e48459098524edc2761ede54457a29
Darwin <darwinllss321@gmail.com>
2023-05-10 17:22:41 -0700

    added tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,43 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+    public void testFilter(){
+        StringChecker sc = new StringChecker(){
+            public boolean checkString(String s){
+                return s.startsWith("w");
+            }
+        };
+        List<String> input1 = new ArrayList<>();
+        input1.add("what");
+        input1.add("what");
+        input1.add("wow");
+        input1.add("bruh");
+        List<String> expected1 = new ArrayList<>();
+        expected1.add("what");
+        expected1.add("what");
+        expected1.add("wow");
+        assertEquals(expected1, ListExamples.filter(input1,sc));
+    }
+
+    @Test
+    public void testMerge(){
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        input1.add("c");
+        List<String> input2 = new ArrayList<>();
+        input2.add("c");
+        input2.add("d");
+        input2.add("e");
+        List<String> expected1 = new ArrayList<>();
+        expected1.add("a");
+        expected1.add("b");
+        expected1.add("c");
+        expected1.add("c");
+        expected1.add("d");
+        expected1.add("e");
+        assertEquals(expected1,ListExamples.merge(input1,input2));
+    }
 }
