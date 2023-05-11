6ddb349e16c0b490c6703a8ed69de5952ca69fd0
Darwin <darwinllss321@gmail.com>
2023-05-10 18:41:03 -0700

    updated

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,7 +8,34 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if 
+if [[ ! -f student-submission/ListExamples.java ]] 
+then 
+    echo "not correct file"
+    exit
+fi 
+
+filedir=`find -name "ListExamples.java"`
+cp -r $filedir grading-area/
+cp -r TestListExamples.java grading-area/
+cp -r lib grading-area/
+
+cd grading-area
+javac -cp $CPATH TestListExamples.java ListExamples.java 2> compile-error.txt
+if [[ $? -ne 0 ]] 
+then 
+    echo "compile failed"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+grep -oP '(?<=Failures: )\d+' test-output.txt
+
+possibleScore=3
+if [[ grep -q "Failures" test-output.txt ]]
+then 
+    result=$((possibleScore - $(echo grep -oP '(?<=Failures: )\d+' test-output.txt)))
+    echo "Your score: $result/3"
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
